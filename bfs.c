31280fd678307d7274360449db0b6ecd056e0f2d
Haitham Naciri ben saghir <hnaciri-@e1r12p2.1337.ma>
2024-01-05 15:17:45 +0100

    All path detected

--- bfs.c
+++ bfs.c
@@ -6,7 +6,7 @@
 /*   By: hnaciri- <hnaciri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 19:37:20 by hnaciri-          #+#    #+#             */
-/*   Updated: 2024/01/04 19:38:56 by hnaciri-         ###   ########.fr       */
+/*   Updated: 2024/01/05 15:16:41 by hnaciri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,27 @@ void	bfs() {
 
 	while (!queueIsEmpty(g_data.queue)) {
         t_room *currentRoom = dequeue(g_data.queue);
-        if (currentRoom == g_data.end) {
-			printf ("Arrived first: %s\n", currentRoom->parent->name);
-            break ;
-        }
         for (int i = 0; i < currentRoom->numberOfNeighbors; i++) {
             t_room *neighbor = currentRoom->neighbors[i];
-            if (neighbor->visited == 0) {
+			if (neighbor == g_data.end) {
+				printf ("Arrived: %s\n", currentRoom->name);
+			}
+            else if (neighbor->visited == 0) {
                 neighbor->visited = 1;
                 neighbor->parent = currentRoom;
                 enqueue(g_data.queue, neighbor);
             }
         }
     }
-    if (g_data.end->parent == NULL) {
-        printf("No path exists between start and end rooms.\n");
-    } else {
-        printf("Shortest path from start to end:\n");
-        t_room *current = g_data.end;
-        while (current != NULL) {
-            printf("%s -> ", current->name);
-            current = current->parent;
-        }
-        printf("NULL\n");
-    }
+    // if (g_data.end->parent == NULL) {
+    //     printf("No path exists between start and end rooms.\n");
+    // } else {
+    //     printf("Shortest path from start to end:\n");
+    //     t_room *current = g_data.end;
+    //     while (current != NULL) {
+    //         printf("%s -> ", current->name);
+    //         current = current->parent;
+    //     }
+    //     printf("NULL\n");
+    // }
 }

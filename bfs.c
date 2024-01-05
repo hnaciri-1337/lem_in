001bf12b2fc49201d8e85bb0709cc64329a1cf55
hnaciri-1337 <haithamnaciribensaghir@gmail.com>
2024-01-05 13:34:07 +0100

    .

--- bfs.c
+++ bfs.c
@@ -18,8 +18,10 @@ void	bfs() {
 
 	while (!queueIsEmpty(g_data.queue)) {
         t_room *currentRoom = dequeue(g_data.queue);
-        if (currentRoom == g_data.end)
+        if (currentRoom == g_data.end) {
 			printf ("Arrived first: %s\n", currentRoom->parent->name);
+            break ;
+        }
         for (int i = 0; i < currentRoom->numberOfNeighbors; i++) {
             t_room *neighbor = currentRoom->neighbors[i];
             if (neighbor->visited == 0) {
@@ -28,5 +30,16 @@ void	bfs() {
                 enqueue(g_data.queue, neighbor);
             }
         }
-    }	
+    }
+    if (g_data.end->parent == NULL) {
+        printf("No path exists between start and end rooms.\n");
+    } else {
+        printf("Shortest path from start to end:\n");
+        t_room *current = g_data.end;
+        while (current != NULL) {
+            printf("%s -> ", current->name);
+            current = current->parent;
+        }
+        printf("NULL\n");
+    }
 }

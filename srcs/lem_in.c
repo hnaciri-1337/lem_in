d8686d387ef49a68b39f992f3d63903fb7e3f58b
Haitham Naciri ben saghir <hnaciri-@e1r12p4.1337.ma>
2024-01-09 18:35:09 +0100

    add header

--- srcs/lem_in.c
+++ srcs/lem_in.c
@@ -6,22 +6,13 @@
 /*   By: hnaciri- <hnaciri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 17:25:27 by hnaciri-          #+#    #+#             */
-/*   Updated: 2024/01/08 15:52:02 by hnaciri-         ###   ########.fr       */
+/*   Updated: 2024/01/09 18:32:39 by hnaciri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
 t_global	g_data;
-
-// void	debug() {
-// 	for (int i = 0; i < g_data.numberOfAvailablePaths; i++) {
-// 		printf ("Length: %d | Assigned: %d | Sum: %d\n", g_data.paths[i].length, g_data.paths[i].antsInPath, g_data.paths[i].length + g_data.paths[i].antsInPath);
-// 		for (int j = 0; g_data.paths[i].path[j]; j++)
-// 			printf ("%s -> ", g_data.paths[i].path[j]->name);
-// 		printf ("\n");
-// 	}
-// }
 
 int main(int ac, char **av) {
 	if (ac != 2)
--- srcs/result.c
+++ srcs/result.c
@@ -6,7 +6,7 @@
 /*   By: hnaciri- <hnaciri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 13:42:03 by hnaciri-          #+#    #+#             */
-/*   Updated: 2024/01/09 17:19:39 by hnaciri-         ###   ########.fr       */
+/*   Updated: 2024/01/09 18:33:27 by hnaciri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,6 @@ void	printResult() {
 			g_data.paths[i].path[j]->antId = -1;
 
 	assignAntsToPath();
-	g_data.numberOfRemainAnts = g_data.numberOfAnts;
-	for (int i = 0; i < g_data.numberOfAvailablePaths; i++) {
-		if (g_data.paths[i].antsInPath) {
-			g_data.paths[i].antsInPath--;
-			g_data.numberOfRemainAnts--;
-			g_data.paths[i].path[0]->antId = g_data.numberOfAnts - g_data.numberOfRemainAnts;
-			ft_putchar_fd('L', 1);
-			ft_putnbr_fd(g_data.paths[i].path[0]->antId, 1);
-			ft_putchar_fd('-', 1);
-			ft_putstr_fd(g_data.paths[i].path[0]->name, 1);
-			ft_putchar_fd(' ', 1);
-		}
-		else {
-			g_data.numberOfAvailablePaths = i + 1;
-			break ;
-		}
-	}
-	ft_putchar_fd('\n', 1);
 
 	while (g_data.numberOfArrivedAnts < g_data.numberOfAnts) {
 		for (int i = 0; i < g_data.numberOfAvailablePaths; i++) {
@@ -57,13 +39,13 @@ void	printResult() {
 				if (g_data.paths[i].path[j - 1]->antId != -1) {
 					g_data.paths[i].path[j]->antId = g_data.paths[i].path[j - 1]->antId;
 					g_data.paths[i].path[j - 1]->antId = -1;
+					if (g_data.paths[i].path[j] == g_data.end)
+						g_data.numberOfArrivedAnts++;
 					ft_putchar_fd('L', 1);
 					ft_putnbr_fd(g_data.paths[i].path[j]->antId, 1);
 					ft_putchar_fd('-', 1);
 					ft_putstr_fd(g_data.paths[i].path[j]->name, 1);
 					ft_putchar_fd(' ', 1);
-					if (g_data.paths[i].path[j] == g_data.end)
-						g_data.numberOfArrivedAnts++;
 				}
 			}
 		}
